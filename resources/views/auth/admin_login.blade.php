51155420547e3b92ea5407b34dd654860a6b0585
MHaidar <muhammadhaidar159@gmail.com>
2026-05-01 17:21:25 +0700

    ubah logo tab pada login admin dan user

--- resources/views/auth/admin_login.blade.php
+++ resources/views/auth/admin_login.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Portal Admin - UKAICHAMPION APP</title>
+    <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
     <script src="https://cdn.tailwindcss.com"></script>
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
 </head>
